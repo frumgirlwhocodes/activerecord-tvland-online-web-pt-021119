ca20c99efa833e2d5330850b280341a12923d060
lieby weintraub <liebahinda@gmail.com>
2019-05-13 21:25:36 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,6 +3,7 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters 
   
   def full_name 
-    return "#{self.first_name} "
+    return "#{self.first_name} #{self.last_name}"
+  end 
   
 end
