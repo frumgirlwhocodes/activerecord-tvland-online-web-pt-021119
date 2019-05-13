abb0dcea791b544d488e036713b2ee94d09fd2f7
lieby weintraub <liebahinda@gmail.com>
2019-05-13 20:58:02 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_actor_to_characters.rb
+++ db/migrate/006_add_actor_to_characters.rb
@@ -1,3 +1,5 @@
 class AddActorToCharacters < ActiveRecord::Migration[4.2]
   def change 
-    add_column :characters, :actor_id, :integer
+    add_column :characters, :actor_id, :integer 
+  end 
+end 
